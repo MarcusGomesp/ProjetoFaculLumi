e59c0561345443dd3959bc802348e5069a2aa151
Marcus Vinicius <marcus_1609@outlook.com>
2025-09-30 21:42:46 -0300

    Ajuste: adicionando .gitignore para ignorar arquivos temporários

--- ProjetoFaculdade6Semestre/ProjetoFaculdade6Semestre/Interface/AI/IOpenAIService.cs
+++
@@ -1,9 +0,0 @@
-﻿using ProjetoFaculdade6Semestre.Model.CadastroLumi;
-
-namespace ProjetoFaculdade6Semestre.Interface.AI
-{
-    public interface IOpenAIService
-    {
-        Task<Result> AnalisarCurriculoAsync(string texto, int roleId, int cvId);
-    }
-}
--- ProjetoFaculdade6Semestre/ProjetoFaculdade6Semestre/Model/DTO/CandidaturaDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ProjetoFaculdade6Semestre.Model.DTO
-{
-    public class CandidaturaDTO
-    {
-        public string Email { get; set; } = string.Empty;
-        public string Vaga { get; set; } = string.Empty;
-    }
-}
